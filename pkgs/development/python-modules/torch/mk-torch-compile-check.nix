4f2e546a160444a9878a271eeacf642bac67b3c2
Mika Tammi <mikatammi@gmail.com>
2024-10-28 01:56:22 +0200

    python3Packages.torch.tests: tests for darwin
    On darwin, both CPU and MPS are tested now.
    
    Signed-off-by: Mika Tammi <mikatammi@gmail.com>

--- pkgs/development/python-modules/torch/mk-torch-compile-check.nix
+++ pkgs/development/python-modules/torch/mk-torch-compile-check.nix
@@ -20,11 +20,16 @@ in
       "${lib.getBin stdenv.cc}/bin"
     ];
   }
+  (
   ''
     import torch
 
 
+  '' +
+    # torch.compile requires OpenMP which is not available on Darwin
+    lib.optionalString (!stdenv.hostPlatform.isDarwin) ''
     @torch.compile
+  '' + ''
     def opt_foo2(x, y):
         a = torch.sin(x)
         b = torch.cos(y)
@@ -35,4 +40,18 @@ in
       opt_foo2(
         torch.randn(10, 10${deviceStr}),
         torch.randn(10, 10${deviceStr})))
+  '' +
+    # MPS is built by default on Darwin, so test it.
+    lib.optionalString stdenv.hostPlatform.isDarwin ''
+    assert torch.backends.mps.is_built(), "PyTorch not build with MPS enabled"
+    if not torch.backends.mps.is_available():
+        print("MPS not available because the current MacOS version is not 12.3+ "
+              "and/or you do not have an MPS-enabled device on this machine.")
+
+    else:
+        print(
+          opt_foo2(
+            torch.randn(10, 10, device="mps"),
+            torch.randn(10, 10, device="mps")))
   ''
+  )
