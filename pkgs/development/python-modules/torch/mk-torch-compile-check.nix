3c6c2a3f6fd4e59f47005f62dfd6102d021d3aa4
Mika Tammi <mikatammi@gmail.com>
2024-11-03 20:53:43 +0200

    python3Packages.torch.tests: Fix formatting
    Signed-off-by: Mika Tammi <mikatammi@gmail.com>

--- pkgs/development/python-modules/torch/mk-torch-compile-check.nix
+++ pkgs/development/python-modules/torch/mk-torch-compile-check.nix
@@ -21,37 +21,40 @@ in
     ];
   }
   (
-  ''
-    import torch
+    ''
+      import torch
 
 
-  '' +
-    # torch.compile requires OpenMP which is not available on Darwin
-    lib.optionalString (!stdenv.hostPlatform.isDarwin) ''
-    @torch.compile
-  '' + ''
-    def opt_foo2(x, y):
-        a = torch.sin(x)
-        b = torch.cos(y)
-        return a + b
+    ''
+    +
+      # torch.compile requires OpenMP which is not available on Darwin
+      lib.optionalString (!stdenv.hostPlatform.isDarwin) ''
+        @torch.compile
+      ''
+    + ''
+      def opt_foo2(x, y):
+          a = torch.sin(x)
+          b = torch.cos(y)
+          return a + b
 
 
-    print(
-      opt_foo2(
-        torch.randn(10, 10${deviceStr}),
-        torch.randn(10, 10${deviceStr})))
-  '' +
-    # MPS is built by default on Darwin, so test it.
-    lib.optionalString stdenv.hostPlatform.isDarwin ''
-    assert torch.backends.mps.is_built(), "PyTorch not build with MPS enabled"
-    if not torch.backends.mps.is_available():
-        print("MPS not available because the current MacOS version is not 12.3+ "
-              "and/or you do not have an MPS-enabled device on this machine.")
+      print(
+        opt_foo2(
+          torch.randn(10, 10${deviceStr}),
+          torch.randn(10, 10${deviceStr})))
+    ''
+    +
+      # MPS is built by default on Darwin, so test it.
+      lib.optionalString stdenv.hostPlatform.isDarwin ''
+        assert torch.backends.mps.is_built(), "PyTorch not build with MPS enabled"
+        if not torch.backends.mps.is_available():
+            print("MPS not available because the current MacOS version is not 12.3+ "
+                  "and/or you do not have an MPS-enabled device on this machine.")
 
-    else:
-        print(
-          opt_foo2(
-            torch.randn(10, 10, device="mps"),
-            torch.randn(10, 10, device="mps")))
-  ''
+        else:
+            print(
+              opt_foo2(
+                torch.randn(10, 10, device="mps"),
+                torch.randn(10, 10, device="mps")))
+      ''
   )
