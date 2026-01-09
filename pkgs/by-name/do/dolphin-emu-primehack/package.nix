6019bbf2f0ac73c711a41f574365acc1ca4b8c4d
Mika Tammi <mikatammi@gmail.com>
2026-01-09 16:44:49 +0200

    WIP: Dolphin hacking
    Signed-off-by: Mika Tammi <mikatammi@gmail.com>

--- pkgs/by-name/do/dolphin-emu-primehack/package.nix
+++ pkgs/by-name/do/dolphin-emu-primehack/package.nix
@@ -7,12 +7,14 @@
   pkg-config,
   cmake,
   qt6,
+  # darwin-only
+  xcbuild,
 
   # buildInputs
   curl,
   enet,
   ffmpeg,
-  fmt_9,
+  fmt,
   gettext,
   libGL,
   libGLU,
@@ -45,6 +47,7 @@
   # darwin-only
   hidapi,
   libpng,
+  moltenvk,
 
   # passthru
   testers,
@@ -64,26 +67,31 @@ stdenv.mkDerivation (finalAttrs: {
   };
 
   patches = [
-    (fetchpatch2 {
-      url = "https://github.com/dolphin-emu/dolphin/commit/8edef722ce1aae65d5a39faf58753044de48b6e0.patch?full_index=1";
-      hash = "sha256-QEG0p+AzrExWrOxL0qRPa+60GlL0DlLyVBrbG6pGuog=";
-    })
+    # (fetchpatch2 {
+    #   url = "https://github.com/dolphin-emu/dolphin/commit/8edef722ce1aae65d5a39faf58753044de48b6e0.patch?full_index=1";
+    #   hash = "sha256-QEG0p+AzrExWrOxL0qRPa+60GlL0DlLyVBrbG6pGuog=";
+    # })
+    ./0001-Need-private-component-also-for-Darwin.patch
+    ./0001-fmt-Replace-deprecated-fmt-localtime-usage-with-Comm.patch
+    ./0001-Fix-unknown-type-NSView.patch
   ];
 
   nativeBuildInputs = [
     pkg-config
     cmake
-  ]
-  ++ lib.optionals stdenv.hostPlatform.isLinux [
     qt6.wrapQtAppsHook
+  ]
+  ++ lib.optionals stdenv.hostPlatform.isDarwin [
+    xcbuild # for plutil
   ];
 
   buildInputs = [
     curl
     enet
     ffmpeg
-    fmt_9
+    fmt
     gettext
+    libao
     libGL
     libGLU
     libSM
@@ -92,33 +100,33 @@ stdenv.mkDerivation (finalAttrs: {
     libXinerama
     libXrandr
     libXxf86vm
-    libao
     libpthreadstubs
-    libpulseaudio
     libusb1
+    libpng
     mbedtls
     miniupnpc
     openal
     pcre
+    hidapi
     portaudio
     qt6.qtbase
     qt6.qtsvg
     readline
     SDL2
-    sfml
     soundtouch
     xz
+    vulkan-loader
   ]
   ++ lib.optionals stdenv.hostPlatform.isLinux [
     alsa-lib
     bluez
     libevdev
+    libpulseaudio
+    sfml
     udev
-    vulkan-loader
   ]
-  ++ lib.optionals stdenv.hostPlatform.isLinux [
-    hidapi
-    libpng
+  ++ lib.optionals stdenv.hostPlatform.isDarwin [
+    moltenvk
   ];
 
   cmakeFlags = [
@@ -128,6 +136,13 @@ stdenv.mkDerivation (finalAttrs: {
   ]
   ++ lib.optionals stdenv.hostPlatform.isDarwin [
     (lib.cmakeBool "OSX_USE_DEFAULT_SEARCH_PATH" true)
+    (lib.cmakeBool "USE_BUNDLED_MOLTENVK" false)
+    (lib.cmakeBool "MACOS_CODE_SIGNING" false)
+    # Bundles the application folder into a standalone executable, so we cannot devendor libraries
+    (lib.cmakeBool "SKIP_POSTPROCESS_BUNDLE" true)
+    # Needs xcode so compilation fails with it enabled. We would want the version to be fixed anyways.
+    # Note: The updater isn't available on linux, so we don't need to disable it there.
+    (lib.cmakeBool "ENABLE_AUTOUPDATE" false)
   ];
 
   qtWrapperArgs = lib.optionals stdenv.hostPlatform.isLinux [
@@ -141,26 +156,25 @@ stdenv.mkDerivation (finalAttrs: {
   postPatch = ''
     substituteInPlace CMakeLists.txt \
       --replace-fail 'DISTRIBUTOR "None"' 'DISTRIBUTOR "NixOS"'
-  ''
-  + lib.optionalString stdenv.hostPlatform.isDarwin ''
-    substituteInPlace CMakeLists.txt \
-      --replace-fail 'if(NOT APPLE)' 'if(true)' \
-      --replace-fail 'if(LIBUSB_FOUND AND NOT APPLE)' 'if(LIBUSB_FOUND)'
   '';
 
   doInstallCheck = true;
 
   postInstall = ''
-    mv $out/bin/dolphin-emu $out/bin/dolphin-emu-primehack
     mv $out/bin/dolphin-emu-nogui $out/bin/dolphin-emu-primehack-nogui
+    mv $out/bin/dolphin-tool $out/bin/dolphin-tool-primehack
+  ''
+  + lib.optionalString stdenv.hostPlatform.isLinux ''
+    mv $out/bin/dolphin-emu $out/bin/dolphin-emu-primehack
     mv $out/share/applications/dolphin-emu.desktop $out/share/applications/dolphin-emu-primehack.desktop
     mv $out/share/icons/hicolor/256x256/apps/dolphin-emu.png $out/share/icons/hicolor/256x256/apps/dolphin-emu-primehack.png
     substituteInPlace $out/share/applications/dolphin-emu-primehack.desktop \
       --replace-fail 'dolphin-emu' 'dolphin-emu-primehack' \
       --replace-fail 'Dolphin Emulator' 'PrimeHack'
-  ''
-  + lib.optionalString stdenv.hostPlatform.isLinux ''
     install -D $src/Data/51-usb-device.rules $out/etc/udev/rules.d/51-usb-device.rules
+  ''
+  + lib.optionalString stdenv.hostPlatform.isDarwin ''
+    mv -v Binaries/DolphinQt.app $out/DolphinQt.app
   '';
 
   passthru = {
@@ -178,7 +192,7 @@ stdenv.mkDerivation (finalAttrs: {
     homepage = "https://github.com/shiiion/dolphin";
     description = "Gamecube/Wii/Triforce emulator for x86_64 and ARMv8";
     license = lib.licenses.gpl2Plus;
-    broken = stdenv.hostPlatform.isDarwin;
+    # broken = stdenv.hostPlatform.isDarwin;
     platforms = lib.platforms.unix;
   };
 })
